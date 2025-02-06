dad9b51941df8ad8c479470819b97df2eb608a07
muslum <muslumdogan01@gmail.com>
2025-02-06 03:04:11 +0000

    imageGrid component created

---
+++ src/components/content/page.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ImageGrid from "./imageGrid";
+
+const Content = () => {
+  return (
+    <div className="w-full">
+      <div className="w-full flex justify-center items-center flex-col pt-[300px]  relative">
+        <div
+          className="bg-no-repeat bg-center w-full h-full absolute "
+          style={{
+            backgroundImage: "url(/content/dots.png)",
+          }}
+        ></div>
+        <h1 className="text-[80px] tracking-[-0.02em] text-[#202428] font-medium leading-[96px] text-center">
+          Her sektöre <br /> kolayca entegre <br /> satış çözümü!
+        </h1>
+        <p className="text-center pt-[40px] text-lg leading-[28px] text-[#202428]">
+          Her sektöre uyarlanabilen bu çözümle, satış süreçlerinizi tek
+          platformda yönetin. <br /> Stok takibinden siparişe, her adımda
+          kolaylık sağlar...
+        </p>
+      </div>
+      <ImageGrid />
+    </div>
+  );
+};
+
+export default Content;
