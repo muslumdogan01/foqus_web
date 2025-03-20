097775a9a3e35992af450339a118e128f779fc3a
muslum <muslumdogan01@gmail.com>
2025-03-20 01:09:05 +0000

    some bug fix about images

--- src/components/content/imageGrid.tsx
+++ src/components/content/imageGrid.tsx
@@ -6,56 +6,49 @@ export default function ImageList() {
     {
       src: "/content/content1.png",
       icon: "/icons/icon1.svg",
-      width: 300,
-      height: 500,
+   
       marginTop: "-mt-[13rem]",
       marginLeft: "-ml-[30px]",
     },
     {
       src: "/content/content3.png",
       icon: "/icons/icon2.svg",
-      width: 300,
-      height: 500,
+
       marginTop: "mt-0",
       marginLeft: "ml-[30px]",
     },
     {
       src: "/content/cont5.png",
       icon: "/icons/icon3.svg",
-      width: 300,
-      height: 500,
+  
       marginTop: "mt-[15rem]",
       marginLeft: "ml-[30px]",
     },
     {
       src: "/content/cont7.png",
-      icon: "/icons/nature.svg",
-      width: 300,
-      height: 500,
+      icon: "/icons/icon1.svg",
+ 
       marginTop: "mt-[8rem]",
       marginLeft: "ml-[30px]",
     },
     {
       src: "/content/cont9.png",
-      icon: "/icons/nature.svg",
-      width: 300,
-      height: 500,
+      icon: "/icons/icon1.svg",
+    
       marginTop: "mt-[23rem]",
       marginLeft: "ml-[30px]",
     },
     {
       src: "/content/cont11.png",
-      icon: "/icons/nature.svg",
-      width: 300,
-      height: 500,
+      icon: "/icons/icon1.svg",
+    
       marginTop: "mt-0",
       marginLeft: "mx-[30px]",
     },
     {
       src: "/content/cont5.png",
-      icon: "/icons/nature.svg",
-      width: 300,
-      height: 500,
+      icon: "/icons/icon1.svg",
+   
       marginTop: "-mt-[13rem]",
       marginRight: "-mr-[30px]",
     },
@@ -64,50 +57,49 @@ export default function ImageList() {
   const images2 = [
     {
       src: "/content/cont8.png",
-      width: 300,
-      height: 500,
-      marginTop: "-mt-[590px]",
+      icon: "/icons/icon2.svg",
+  
+      marginTop: "-mt-[560px]",
       marginLeft: "-ml-[30px]",
     },
     {
       src: "/content/cont4.png",
-      width: 300,
-      height: 500,
+      icon: "/icons/icon1.svg",
+   
       marginTop: "-mt-[340px]",
       marginLeft: "ml-[30px]",
     },
     {
       src: "/content/cont6.png",
-      width: 300,
-      height: 500,
+      icon: "/icons/icon1.svg",
+    
       marginTop: "-mt-[100px]",
       marginLeft: "ml-[30px]",
     },
     {
       src: "/content/cont8.png",
-      width: 300,
-      height: 500,
+      icon: "/icons/icon1.svg",
+   
       marginTop: "-mt-[210px]",
       marginLeft: "ml-[30px]",
     },
     {
       src: "/content/cont10.png",
-      width: 300,
-      height: 500,
+      icon: "/icons/icon1.svg",
       marginTop: "mt-[30px]",
       marginLeft: "ml-[30px]",
     },
     {
       src: "/content/cont12.png",
-      width: 300,
-      height: 500,
+      icon: "/icons/icon2.svg",
+   
       marginTop: "-mt-[340px]",
       marginLeft: "mx-[30px]",
     },
     {
       src: "/content/cont14.png",
-      width: 300,
-      height: 500,
+      icon: "/icons/icon3.svg",
+    
       marginTop: "-mt-[540px]",
       marginRight: "-mr-[30px]",
     },
@@ -115,50 +107,77 @@ export default function ImageList() {
 
   return (
     <div className="flex flex-col justify-center items-center w-full relative">
-<div className="inline-flex justify-center z-10 relative">
-  {images.map((image, index) => (
-    <div
-      key={index}
-      className={`group relative ${image.marginTop} ${image.marginLeft} ${image.marginRight}`}
-    >
-      {/* Resim */}
-      <Image
-        src={image.src}
-        width={image.width}
-        height={image.height}
-        alt={`Image ${index + 1}`}
-        className="rounded-2xl shadow-lg object-cover transition duration-300 ease-in-out group-hover:blur-[10px]"
-      />
-
-      {/* Hover Olunca Görünecek Dinamik İkon */}
-      <span className="absolute top-0 left-0 w-20 h-20 bg-black/50 rounded-2xl opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out flex items-center justify-center">
-        <Image
-          src={image.icon} // Dinamik olarak resme ait ikon
-          width={40}
-          height={40}
-          alt={`Icon for ${index + 1}`}
-          className="object-contain"
-        />
-      </span>
-    </div>
-  ))}
-</div>
-      <div className="flex w-full justify-center z-10">
-        {images2.map((image, index) => (
-          <div
-            key={index}
-            className={`group ${image.marginTop} ${image.marginLeft} ${image.marginRight}`}
-          >
+    <div className="inline-flex justify-center z-10 relative">
+      {images.map((image, index) => (
+        <div
+          key={index}
+          className={`group relative ${image.marginTop} ${image.marginLeft} ${image.marginRight}`}
+        >
+          {/* Resmi ve hover efektini içine alan relative div */}
+          <div className="relative 2xl:w-[250px] 2xl:h-[400px]  w-[200px] h-[300px]">
+            {/* Resim */}
             <Image
               src={image.src}
-              width={image.width}
-              height={image.height}
+            fill
               alt={`Image ${index + 1}`}
-              className="rounded-2xl shadow-lg object-cover transition duration-300 ease-in-out group-hover:blur-[10px]"
+          
+              className=" rounded-2xl shadow-lg object-cover transition duration-300 ease-in-out group-hover:blur-[10px] w-full h-full"
             />
+  
+            {/* Hover İçeriği (İkon + Metin) */}
+            <span className="absolute inset-0 flex flex-col items-center justify-center bg-black/50 rounded-2xl opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
+              {/* İkon */}
+              <Image
+                src={image.icon}
+                width={40}
+                height={40}
+                alt={`Icon for ${index + 1}`}
+                className="object-contain"
+              />
+  
+              {/* Açıklama Metni */}
+              <span className="text-white text-[18px] md:text-[24px] leading-[1.4] tracking-[-0.02em] mt-2 text-center">
+                Güzellik <br /> salonları <br /> için
+              </span>
+            </span>
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
     </div>
+  
+    <div className="inline-flex justify-center z-10 relative">
+      {images2.map((img, index) => (
+        <div
+          key={index}
+          className={`group relative ${img.marginTop} ${img.marginLeft} ${img.marginRight}`}
+        >
+ <div className="relative 2xl:w-[250px] 2xl:h-[400px]  w-[200px] h-[300px]">
+            {/* Resim */}
+            <Image
+              src={img.src}
+       fill
+              alt={`Image ${index + 1}`}
+              className="rounded-2xl shadow-lg object-cover transition duration-300 ease-in-out group-hover:blur-[10px] w-full h-full"
+            />
+  
+            {/* Hover İçeriği */}
+            <span className="absolute inset-0 flex flex-col items-center justify-center bg-black/50 rounded-2xl opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
+              <Image
+                src={img.icon}
+                width={40}
+                height={40}
+                alt={`Icon for ${index + 1}`}
+                className="object-contain"
+              />
+              <span className="text-white text-[18px] md:text-[24px] leading-[1.4] tracking-[-0.02em] mt-2 text-center">
+                Güzellik <br /> salonları <br /> için
+              </span>
+            </span>
+          </div>
+        </div>
+      ))}
+    </div>
+  </div>
+  
   );
 }
