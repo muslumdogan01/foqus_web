23e5eed612c45080835b2fae19eb62d16ef06765
muslum <muslumdogan01@gmail.com>
2025-04-07 22:02:01 +0100

    bg

--- src/components/bg/page.tsx
+++ src/components/bg/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const BackgroundImage = () => {
   return (
     <div
-      className=" absolute right-[-40px] 2xl:-right-40 top-0 w-[50%] xl:w-[55%] h-full  bg-no-repeat bg-cover bg-center"
+      className=" absolute right-[-40px] 2xl:-right-40 top-0 w-[50%] h-full  bg-no-repeat bg-cover bg-center"
       style={{
         backgroundImage: "url(/header/phone2.png)",
  
